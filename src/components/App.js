ebb3faef9c53296bc5c18277e4a0dd3af59109b1
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-08-29 10:21:42 +0530

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  // Step 1: Initialize counter state
+  const [count, setCount] = useState(0);
+
+  // Step 2: Handle button click
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      
+      <p>Button clicked {count} times</p>
 
-export default App
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default App;
